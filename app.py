7193f5c5f43cd1c3624c6925fb8a34ec493eefcd
Jestin Gigi <75965382+JestinGigi@users.noreply.github.com>
2023-07-19 21:39:16 +0530

    Added StockVis Dash Files

---
+++ app.py
@@ -0,0 +1,205 @@
+from dash import Dash, dcc, html, Input, Output, State, callback
+from datetime import datetime as dt
+
+#Imports for plotting
+import yfinance as yf 
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+
+# Model
+from model import forecastPrices
+
+# Dash Instance
+app = Dash(__name__, meta_tags=[{'name': 'viewport','content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}])
+
+# Server Property
+server = app.server
+
+item1 = html.Div(
+    [
+        html.Div([
+            html.P("StockVis Navigation", className="start"),
+        html.Div([
+            #stock code input
+            html.P([html.A("Click Me for Stock Code", href='https://stockanalysis.com/stocks/', target='_blank')]),
+            
+            dcc.Input(
+                id='stock',
+                type='text',
+                placeholder='Stock Code',
+                className='btn',
+                required=True,
+                # list=['aapl', 'goog', 'amzn', 'tsla']
+            ),
+            html.Button('Submit', id='btn-submit', n_clicks=0, className='btn'),
+        ], className='stock-text'), 
+        html.Div([
+            # Date range picker input
+            dcc.DatePickerRange(
+            id='my-date-picker-range',
+            start_date=dt(2017,1, 21),
+            end_date=dt.now(),
+            max_date_allowed=dt.now(),
+            initial_visible_month=dt.now(),
+    ),
+        ]),
+        html.Div([
+            #Stock price button
+            html.Div([
+                html.Button('Stock Price', id='btn-stock', className='btn btn-lower'),
+            #indicator button
+                html.Button('Indicator', id='btn-indicator', className='btn btn-lower'),
+            ], className='price'),
+            #Number of days of forecast input
+            html.Div([
+                dcc.Input(
+                id='days',
+                placeholder='Days',
+                type='number',
+                className='btn btn-lower',
+                min=5,
+                max=60,
+            ),
+            #forecast button
+            html.Button('Forecast', id='btn-forecast', className='btn btn-lower')], className='forecast'),
+            
+        ], className='fields')
+    ], className='inputs')
+    ],
+    className='nav')
+
+item2 = html.Div(
+    [  html.Div([
+            html.Div([
+                html.Div(
+                    [
+                        #Logo
+                        #Company name
+                        html.H2(children='Stocks', id='company-header')
+                    ],className='header'),
+                
+                html.Div([''], id='Description', className='description_ticker'),
+                
+                html.Div(
+                    #Stock price plot
+                 id='graph-content'),
+                
+                html.Div(
+                    #indicator plot
+                 id='indicator-content'),
+                
+                html.Div(
+                    #forecast plot
+                id='forecast-content')
+            ], className='main-content')
+        ], className='inner-content')
+    ], className='content')
+
+
+app.layout = html.Div([item1, item2], className='container')
+
+
+# Updating stock header and description 
+@callback(Output('company-header', 'children'),
+    Output('Description', 'children'),
+    Input('btn-submit', 'n_clicks'),
+    State('stock', 'value'))
+def update_data(n_clicks, input1):
+    # your function here
+    if (n_clicks != 0):
+        try:
+            ticker = yf.Ticker(input1)
+            inf = ticker.info
+            df = pd.DataFrame().from_dict(inf, orient="index").T
+            shortname = df['shortName'][0]
+            summary = df['longBusinessSummary'][0]
+            return shortname, summary
+        except:
+            return 'Something went Wrong', 'Stock Code Incorrect/Required'
+    return 'Stocks', ''
+
+# Setting stock price graph
+def get_stock_price_fig(df):
+    fig = px.line(df,
+                    x= 'Date',
+                    y= ['Open', 'Close'],
+                    template='plotly_dark',
+                    title="Closing and Opening Price vs Date",
+                    labels={
+                        'value':'Price','variable':'Price Type'})
+    return fig
+
+@callback(Output('graph-content', 'children'),
+          Input('btn-stock', 'n_clicks'),
+          State('stock', 'value'),
+          State('my-date-picker-range', 'start_date'),
+          State('my-date-picker-range', 'end_date'))
+def plot_data(n_clicks, input1, start_date, end_date):
+    if (n_clicks is not None):
+        try:
+            df = yf.download(input1, dt.fromisoformat(start_date), dt.fromisoformat(end_date))
+            df.reset_index(inplace=True)
+            fig = get_stock_price_fig(df)
+            return dcc.Graph(responsive=True, figure=fig, config={'scrollZoom': True}, )
+        except:
+            return 'Stock Code is required/Incorrect'
+    return ''
+
+# Setting indicator graph
+def get_more(df):
+    df['EWA_20'] = df['Close'].ewm(span=20, adjust=False).mean()
+    fig = px.scatter(df,
+                    x= 'Date',
+                    y= 'EWA_20',
+                    template='plotly_dark',
+                    title="Exponential Moving Average vs Date")
+    fig.update_traces(mode='lines')
+    return fig
+    
+@callback(Output('indicator-content', 'children'),
+          Input('btn-indicator', 'n_clicks'),
+          State('stock', 'value'),
+          State('my-date-picker-range', 'start_date'),
+          State('my-date-picker-range', 'end_date'))
+def get_indicator(n_clicks, input1, start_date, end_date):
+    if (n_clicks is not None):
+        try:
+            df = yf.download(input1, dt.fromisoformat(start_date), dt.fromisoformat(end_date))
+            df.reset_index(inplace=True)
+            fig = get_more(df)
+            return dcc.Graph(responsive=True, figure=fig, config={'scrollZoom': True})
+        except:
+            return 'Stock Code is required/Incorrect'
+    return ''
+
+
+def plotprices(df, days):
+    fig = px.line(df, 
+                  x='Date', 
+                  y='Close',
+                  template='plotly_dark',
+                  title=f"Predicted prices for {days} days",
+                  labels={
+                      'Close':'Close Price'
+                  })
+    return fig
+@callback(Output('forecast-content', 'children'),
+          Input('btn-forecast', 'n_clicks'),
+          State('days', 'value'),
+          State('stock', 'value'))
+def Predict_Prices(n_clicks, input1, code):
+    if (n_clicks is not None):
+        if (type(input1) is not int):
+            return 'Number of Days required and should be greater than 4'
+        else:
+            try:
+                df = forecastPrices(input1, code)
+                fig = plotprices(df, input1)
+                return dcc.Graph(responsive=True, figure=fig, config={'scrollZoom': True})
+            except:
+                return 'Stock Code is required/Incorrect'
+    return ''
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ model.py
@@ -0,0 +1,78 @@
+# Import necessary libraries
+# import matplotlib.pyplot as plt
+# import seaborn as sn
+import pandas as pd
+import datetime as dt
+import numpy as np
+from sklearn.svm import SVR
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+
+# Metrics
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+# yfinance
+import yfinance as yf
+
+def get_dates(forecast):
+    dates = []
+    start_date = dt.date.today()
+    for i in range(forecast):
+        start_date += dt.timedelta(days=1)
+        dates.append(start_date)
+    return dates
+
+def forecastPrices(days, code):
+    # Previous 30 days data for training
+    ndays = '60d'
+    
+    ticker = yf.Ticker(code)
+    df = ticker.history(period=ndays) 
+    df.reset_index(inplace=True)
+    
+    # Dataframe having Date and Closing price
+    df = df[['Date', 'Close']]
+    
+    #Removing Timestamp from Date
+    df['Date'] = pd.to_datetime(df['Date']).dt.date
+    
+    forecast = days
+    df['Prediction'] = df.Close.shift(-forecast)
+    
+    # Calculated dates of predicted prices
+    idates = get_dates(forecast)
+    
+    #Closing price of with NaN predicted prices
+    iclose = np.array(df['Close'][-forecast:]).reshape(-1,1)
+    
+    fdf = df.iloc[:-forecast]
+    fdf = fdf.drop('Date', axis=1)
+    
+    # Barplot
+    # plt.figure(figsize=(10,6))
+    # sn.boxplot(fdf['Close'])
+    # plt.show()
+    # Q1 = fdf['Close'].quantile(.25)
+    # Q3 = fdf['Close'].quantile(.75)
+    # IQR = Q3 - Q1
+    # fdf[(fdf['Close'] < Q1 - 1.5*IQR) | (fdf['Close'] > Q3 + IQR * 1.5)]
+    
+    x = np.array(fdf['Close'])
+    x = x.reshape(-1,1)
+    y = np.array(fdf['Prediction'])
+    
+    # Splitting dataset into training and testing
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.1)
+    
+    # Parmeter list of gridsearchCV
+    parameter_list = {
+        'C': [1, 10, 100, 1000, 10000],
+        'gamma': [10, 1, .1, .01, .001, .0001]
+    }
+    
+    # Hyperparameter tuning using GridSearchCV
+    svrRbfModel = SVR(kernel='rbf')
+    gridsvr = GridSearchCV(svrRbfModel, parameter_list, cv=None, scoring=['neg_mean_absolute_error', 'neg_mean_squared_error'], refit='neg_mean_absolute_error')
+    gridsvr.fit(x_train, y_train)
+    predictions = gridsvr.predict(iclose)
+    predDict = {'Date': idates, 'Close': predictions}
+    return pd.DataFrame(data=predDict)
